402890e1bb22776bfcc8031bde4a2c377c89c3cf
zhaoqingqing <shzhaoqingqing@corp.netease.com>
2017-11-24 10:45:50 +0800

    no message

---
+++ new.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>
+FIRST PHP
+</title>
+</head>
+<body>
+<?php
+　　echo 'HELLO WORLD'
+       phpinfo();
+?>
+</body>
+</html>
---
+++ node.js
@@ -0,0 +1,17 @@
+
+const path = require('path');
+var myurl=path.basename('/foo/bar/baz/asdf/quux.html')
+console.log(myurl)
+
+
+
+
+
+
+
+//const http=require("http");
+//var server=http.createServer(function(req,res){
+//	res.write("55");
+//
+//res.end()
+//}).listen("8090")
