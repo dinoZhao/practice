49b073f04b7d32edef2b743dc518d93c7a931f8a
zhaoqingqing <shzhaoqingqing@corp.netease.com>
2018-06-28 10:10:14 +0800

    把最初的配置文件删掉，不然碍事

--- webpackandreact/src/index.js
+++ webpackandreact/src/index.js
@@ -1,6 +1,8 @@
+// require('./index.scss')
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App.jsx';
+
 import('./add.js').then(add=>add)
 ReactDOM.render(<App />, document.getElementById('root'));
 
