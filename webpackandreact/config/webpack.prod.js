2bb6936d18f77525f2b534cb6c66c7df058aa680
zhaoqingqing <shzhaoqingqing@corp.netease.com>
2018-06-28 15:57:18 +0800

    no message

--- webpackandreact/config/webpack.prod.js
+++ webpackandreact/config/webpack.prod.js
@@ -1,32 +1,33 @@
 const merge = require('webpack-merge');
-const base=require('./webpack.base');
+const base = require('./webpack.base');
 const path = require('path');
 var CleanWebpackPlugin = require('clean-webpack-plugin')
-module.exports=merge(base,{
-	plugins:[
-    new CleanWebpackPlugin(['../dist'],{allowExternal:true}),
-	
+module.exports = merge(base, {
+	plugins: [
+		new CleanWebpackPlugin(['../dist'], {
+			allowExternal: true
+		}),
+
 	],
-	mode:'production',
-	output:{
-		path:path.join(__dirname,"..",'dist'),
-		filename:'static/js/index.js',
+	mode: 'production',
+	output: {
+		path: path.join(__dirname, "..", 'dist'),
+		filename: 'static/js/index.js',
 		publicPath: './',
 	},
 	module: {
-		rules: [
-			 {
-        test: /\.(png|jpg|svg|gif)/,
-        use:[{
-loader: "file-loader",
-options:{
-	name:"static/images/[hash:8].[name].[ext]",
-	publicPath: '../'
-}
-		}
-         
-        ]
-      }
+		rules: [{
+				test: /\.(png|jpg|svg|gif)/,
+				use: [{
+						loader: "file-loader",
+						options: {
+							name: "static/images/[hash:8].[name].[ext]",
+							publicPath: '../'
+						}
+					}
+
+				]
+			}
 
 		]
 	},
