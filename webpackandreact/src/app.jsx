5e04055388977e03d0d7872f3ac412137e0e67a6
zhaoqingqing <934460790@qq.com>
2018-08-06 08:48:27 +0800

    no message

--- webpackandreact/src/app.jsx
+++ webpackandreact/src/app.jsx
@@ -1,33 +1,54 @@
 import React from 'react';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() }
-  }
-  componentDidMount(){
-    
-setInterval(()=>{
-  this.kick()
-}
-,1000)
-  }
-componentWillUnmount(){
+//class Clock extends React.Component {
+//constructor(props) {
+//  super(props);
+//  this.state = { date: new Date() }
+//}
+//componentDidMount(){
+//  
+//setInterval(()=>{
+//this.kick()
+//}
+//,1000)
+//}
+//componentWillUnmount(){
+//
+//}
+//kick(){
+//this.setState({date:new Date()})
+//}
+//render() {
+//  return (
+//    <div>{this.state.date.toString()}</div>
+//  )
+//}
+//}
+//
+//
+//export default function () {
+//return (
+//  <Clock date={new Date()}></Clock>
+//);
+//}
+var chil="pp"
+var  element = <h1>Hello, world!{chil}</h1>;
 
-}
-kick(){
-  this.setState({date:new Date()})
-}
-  render() {
-    return (
-      <div>{this.state.date.toString()}</div>
-    )
-  }
-}
 
-
-export default function () {
-  return (
-    <Clock date={new Date()}></Clock>
-  );
+function We(){
+	return <h1>Hello, world!{chil}</h1>;
 }
+function foo(){
+	return (
+		<div>
+		<We/>
+		<div>544444</div>
+		</div>
+	)
+}
+//class foo extends React.Component{
+//	render(){
+//	return element
+//	}
+//}
+export default foo;
--- webpackandreact/src/index.js
+++ webpackandreact/src/index.js
@@ -1,9 +1,20 @@
 require('./style/index.scss')
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './App.jsx';
+//import App from './App.jsx';
 import('./add.js').then(add=>add)
 document.getElementById("logo").src='./static/images/logo.png'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
+
+
+
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) =>
+  <li key={number.toString()}>{number}</li>
+);
+
+ReactDOM.render( <ul>{listItems}</ul>, document.getElementById('root'));
+
+  
