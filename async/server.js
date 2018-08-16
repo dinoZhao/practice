63230d92c78b1436ab38bbc00e84e961692ce3e2
zhaoqingqing <934460790@qq.com>
2018-08-16 08:59:03 +0800

    no message

--- async/server.js
+++ async/server.js
@@ -1,5 +1,8 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static('./dist'));
 app.get("/s", function(req, res) {
 
@@ -11,7 +14,10 @@ app.get("/b", function(req, res) {
    
    res.jsonp({ user: 'tobi' })
 });
-
+app.post("/b",function(req,res){
+	console.log(req)
+	res.json(req.body);
+})
 app.listen(3000, function() {
     console.log("App started on port 3000");
 });
