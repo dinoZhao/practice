9ca6e7cf8c6ae8433782f00676f638f181493190
zhaoqingqing <934460790@qq.com>
2018-10-17 16:41:12 +0800

    no message

---
+++ generator/server.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var thunkify = require('thunkify');
+var readFileThunk = thunkify(fs.readFile);
+
+var gen = function* (){
+  var r1 = yield readFileThunk('/etc/fstab');
+  var r2 = yield readFileThunk('/etc/shells');
+};
+
+var g = gen();
+
+var r1 = g.next();
+console.log(r1)
+r1.value(function (err, data) {
+  if (err) throw err;
+  var r2 = g.next(data);
+  r2.value(function (err, data) {
+    if (err) throw err;
+    g.next(data);
+  });
+});
